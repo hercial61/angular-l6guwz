2dcca47ac2e63c634d1e1b58bddbecbd1aa8d706
hercial61 <hercial61@live.com>
2021-03-11 12:07:11 -0800

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
+      {path: 'products/:productId', component:
+      ProductDetailsComponent },
     ])
   ],
   declarations: [
