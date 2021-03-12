05dd484bb03c535dbc2f31e7689c3637f07c3582
hercial61 <hercial61@live.com>
2021-03-11 16:52:15 -0800

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -21,6 +21,9 @@ export class ProductDetailsComponent implements OnInit {
     // First get the product Id from the current Route
     const routeParams = this.route.snapshot.paramMap
     const productIdFromRoute = Number( routeParams.get{'productId '});
+
+    //Find the product that correspond with the id provided in Route
+    this.product = products.find(product => product.id === productIdFromRoute);
   }
 
 }
